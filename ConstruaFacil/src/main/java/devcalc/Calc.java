d54d0619d73516b243ab5b06fab1af42679daf3d
Luana Souza <luananunes.ac@gmail.com>
2022-01-18 17:45:22 -0300

    Finalizado o programa Calculadora, foram feitos melhoria na funcionalidade como: retornar o valor "opção invalida" quando digitar valores < 1 e > 4.

--- ConstruaFacil/src/main/java/devcalc/Calc.java
+++ ConstruaFacil/src/main/java/devcalc/Calc.java
@@ -23,37 +23,38 @@ public class Calc {
         System.out.println("(2) Subtrair");
         System.out.println("(3) Multiplicar");
         System.out.println("(4) Dividir");
-        System.out.println("Escolha o Calculo Desejado");
+        System.out.print("Escolha o Calculo Desejado");
 
         // Recebe a opção desejada
-        String opcao = entrada.nextLine();
+        int opcao = entrada.nextInt();
+        int num1 =0;
+        int num2 =0;
 
         //Pergunta qual a opção usar para o calculo
-        System.out.println("Entre o 1º numero: ");
-        int num1 = entrada.nextInt();
-        System.out.println("Entre o 2º numero: ");
-        int num2 = entrada.nextInt();
 
-        // Chama a função do calculo desejado
-        switch (opcao){
-            case "1":
-                somarDoisNumeros(num1, num2);
+        if (opcao >= 1 && opcao <= 4) {
+            System.out.print("Entre o 1º numero: ");
+            num1 = entrada.nextInt();
+            System.out.print("Entre o 2º numero: ");
+            num2 = entrada.nextInt();// Chama a função do calculo desejado
+            System.out.print("O resultado é: ");
+        }
+
+        switch (opcao) {
+            case 1:
+                System.out.println(somarDoisNumeros(num1, num2));
                 break;
 
             default:
                 System.out.println("Opção invalidada");
                 break;
-
         }
-
-        System.out.println("Passou pelo método main");
-        somarDoisNumeros();
 
     }
 
-    public static void somarDoisNumeros(int num1, int num2){
+    public static int somarDoisNumeros(int num1, int num2) {
+        return num1 + num2;
 
-        System.out.println("Passou pelo método somarDoisNumeros");
 
     }
 
